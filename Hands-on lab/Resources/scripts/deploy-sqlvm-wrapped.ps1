70f5b0e226d4b11a74b619e60e5c3b9522f5457e
Ben Stegink <bstegink@intelligink.com>
2022-04-28 13:07:24 -0400

    removed debug output

--- Hands-on lab/Resources/scripts/deploy-sqlvm-wrapped.ps1
+++ Hands-on lab/Resources/scripts/deploy-sqlvm-wrapped.ps1
@@ -35,13 +35,11 @@ $backups = "F:\Backup"
 [system.io.directory]::CreateDirectory($backups)
 
 Write-Output "Starting SQL"
-Write-Output $sqlservice
 
 # Make sure SQL Service is started
 $sqlservice = Get-Service -Name MSSQLServer
 Start-Service $sqlservice
 $sqlservice.WaitForStatus('Running', '00:01:30')
-Write-Output "SQL should be started or it timed out after 90 seconds"
 Write-Output $sqlservice
 
 # Setup the data, backup and log directories as well as mixed mode authentication
